60c9e3d9845d81f114df88704ef1cc1a418ed842
GunaS911 <gunaswaminathan2567@gmail.com>
2026-02-10 00:12:46 +0530

    Add files via upload

---
+++ Data science/numpy/joinarr.py
@@ -0,0 +1,10 @@
+import numpy as np
+arr1=np.array([1,2,3])
+arr2=np.array([4,5,6])
+print("Concatenating")
+arr3=np.concatenate((arr1,arr2))
+print(arr3)
+print("Vertical Stacking")
+print(np.vstack((arr1,arr2)))
+print("Horizontal Stacking")
+print(np.hstack((arr1,arr2)))
